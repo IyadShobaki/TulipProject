533c153b40e39b7524be3e5edb974e374c36f354
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-30 21:11:38 -0400

    Add WPF UI Project
    - Install and wire up Caliburn.Micro

---
+++ TulipWpfUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TulipWpfUI.ViewModels;
+
+namespace TulipWpfUI
+{
+    // Setup Caliburn.Micro
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        // On startup launch ShellViewModel as base view
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
